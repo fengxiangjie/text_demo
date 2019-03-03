c63fa57529c18e4fda97d930c80dbd957c21c75d
fengxiangjie <13849753353@163.com>
2019-03-03 23:23:59 +0800

    wewqe

--- Singleton.java
+++ Singleton.java
@@ -7,15 +7,15 @@ public class Singleton {
 
 
     //饿汉式
-   /* private Singleton(){};
+    private Singleton(){};
     private static Singleton singleton = new Singleton();
     public static Singleton getSingleton(){
         return singleton;
-    }*/
+    }
 
 
-    //懒汉式
-    /*private Singleton(){};
+   /* //懒汉式
+    private Singleton(){};
     private static Singleton singleton = null;
     public static Singleton getSingleton(){
         if(singleton==null){
@@ -24,19 +24,7 @@ public class Singleton {
         return singleton;
     }*/
 
-    //线程安全
-    private Singleton(){};
-    private static Singleton singleton;
-    public static Singleton getSingleton(){
-        if(null==singleton){
-            synchronized (singleton){
-                if(singleton==null){
-                    singleton = new Singleton();
-                }
-            }
-        }
-        return singleton;
-    }
+
 
 
 
